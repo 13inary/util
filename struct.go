60bd66e8b3b2c8f28465e6c87c16fa971b0879ed
iamclient <936095897@qq.com>
2024-10-23 19:45:15 +0800

    优化：模型转换

--- struct.go
+++ struct.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 )
 
-func ConvertModel(srcModel any, srcModelPointer any) error {
+// ConvertModel 源模型的字段会覆盖目标模型的字段。注意转换多个源到目的模型时多个源的字段相同问题。
+func ConvertModel(srcModel any, dstModelPointer any) error {
 	srcBytes, err := json.Marshal(srcModel)
 	if err != nil {
 		return fmt.Errorf("failed to json.Marshal %+v, %s", srcModel, err.Error())
 	}
-	if err := json.Unmarshal(srcBytes, srcModelPointer); err != nil {
-		return fmt.Errorf("failed to json.Unmarshal from %+v to %+v, %s", string(srcBytes), srcModelPointer, err.Error())
+	if err := json.Unmarshal(srcBytes, dstModelPointer); err != nil {
+		return fmt.Errorf("failed to json.Unmarshal from %+v to %+v, %s", string(srcBytes), dstModelPointer, err.Error())
 	}
 	return nil
 }
