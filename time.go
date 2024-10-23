c6c68dfe6d82f393922d849c6b3693595fd0868a
iamclient <936095897@qq.com>
2024-10-23 19:28:14 +0800

    添加：时区设置

--- time.go
+++ time.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+func InitTimezone() {
+	time.Local = time.FixedZone("CST", 8*3600) // 东八区
+}
+
 // LoopExecute 2次逻辑执行相隔的时间为：逻辑的执行时间 + 间隔时间
 func LoopExecute(ctx context.Context, job func() error, interval time.Duration) error {
 	timer := time.NewTimer(0)
