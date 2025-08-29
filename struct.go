5ce221c7508f0a2d09a21804ff5d3cb4fc2bba9e
iamclient <936095897@qq.com>
2025-08-29 21:16:14 +0800

    optimize

--- struct.go
+++ struct.go
@@ -22,7 +22,7 @@ func ConvertModel(srcModel any, dstModelPointer any) error {
 
 // Map2Struct 对于结构体字段名，若map中有就拿过来覆盖。
 // 对于嵌套结构体的问题：调用方传入嵌套的字段。
-func Map2Struct(srcMap map[string]any, dstStructPointer interface{}) error {
+func Map2Struct(srcMap map[string]any, dstStructPointer any) error {
 	// 获取结构体类型和字段
 	val := reflect.ValueOf(dstStructPointer).Elem()
 	typ := val.Type()
