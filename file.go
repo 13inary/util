301f6b1f922b87b87d7cfc004350cc84b9286988
iamclient <936095897@qq.com>
2025-09-26 01:48:53 +0800

    file: 添加写小文件函数

--- file.go
+++ file.go
@@ -1,6 +1,8 @@
 package util
 
-import "os"
+import (
+	"os"
+)
 
 // FileExists 检查指定路径的文件是否存在
 // 参数:
@@ -20,4 +22,35 @@ func FileExists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+// 异步写文件。写完文件并关闭后，若在操作系统同步文件前断电，会丢失文件。
+func AtomicWriteSmallFile(file string, content []byte, perm os.FileMode) error {
+	tmpName := file + ".tmp"
+	if err := os.WriteFile(tmpName, content, perm); err != nil { // 覆盖式写入
+		return err
+	}
+	return os.Rename(tmpName, file) // 对于目标已经存在，linux和mac直接覆盖，windows需要自己先删除后Rename
+}
+
+// 同步写文件。写完文件后，让操作系统同步文件，然后再关闭文件，避免文件的丢失。
+// 同步操作会损耗性能
+func SyncAtomicWriteSmallFile(file string, content []byte, perm os.FileMode) error {
+	tmpName := file + ".tmp"
+
+	f, err := os.OpenFile(tmpName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, perm)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err = f.Write(content); err != nil {
+		return err
+	}
+
+	if err := f.Sync(); err != nil {
+		return err
+	}
+
+	return os.Rename(tmpName, file) // 对于目标已经存在，linux和mac直接覆盖，windows需要自己先删除后Rename
 }
