604019cc9af84596ac50f6330c392b74a88f9a51
iamclient <936095897@qq.com>
2025-10-17 01:13:09 +0800

    LoopExecute: 添加准备时间

--- time.go
+++ time.go
@@ -11,8 +11,8 @@ func InitTimezone() {
 }
 
 // LoopExecute 2次逻辑执行相隔的时间为：逻辑的执行时间 + 间隔时间
-func LoopExecute(ctx context.Context, job func() error, interval time.Duration) error {
-	timer := time.NewTimer(0)
+func LoopExecute(ctx context.Context, job func() error, prepare time.Duration, interval time.Duration) error {
+	timer := time.NewTimer(prepare)
 	defer timer.Stop()
 	for {
 		select {
